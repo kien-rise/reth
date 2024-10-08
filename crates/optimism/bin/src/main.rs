52ab3cf7b97ce95bf9f8ee2a1f891532f17fa2e3
Kien Nguyen <kien@caliber.build>
2024-10-09 01:30:42 +0700

    feat: clean up Cargo.toml files

--- crates/optimism/bin/src/main.rs
+++ crates/optimism/bin/src/main.rs
@@ -11,6 +11,7 @@ use reth_optimism_rpc::SequencerClient;
 use reth_provider::providers::BlockchainProvider2;
 
 use tracing as _;
+use pevm as _;
 
 #[global_allocator]
 static ALLOC: reth_cli_util::allocator::Allocator = reth_cli_util::allocator::new_allocator();
