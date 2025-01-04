323dd66d09e0d84572c097b6ad3cbca2713d1b65
Kien Nguyen <kien@riselabs.xyz>
2025-01-04 14:37:24 +0700

    DEBUG: print hit or miss

--- crates/trie/db/src/hashed_cursor.rs
+++ crates/trie/db/src/hashed_cursor.rs
@@ -127,9 +127,11 @@ where
     ) -> Result<Option<(B256, Self::Value)>, reth_db::DatabaseError> {
         if let Some((req, res)) = self.last_query {
             if req <= subkey && res.is_none_or(|res| subkey <= res.key) {
+                println!("127f7574 seek: HIT");
                 return Ok(res.map(|e| (e.key, e.value)));
             }
         }
+        println!("127f7574 seek: MISS");
         let entry = self.cursor.seek_by_key_subkey(self.hashed_address, subkey)?;
         self.last_query = Some((subkey, entry));
         Ok(entry.map(|e| (e.key, e.value)))
@@ -137,9 +139,11 @@ where
 
     fn next(&mut self) -> Result<Option<(B256, Self::Value)>, reth_db::DatabaseError> {
         let Some((_, Some(res))) = self.last_query else {
+            println!("127f7574 next: HIT-1");
             return Ok(None);
         };
         let Some(new_req) = increment(res.key) else {
+            println!("127f7574 next: HIT-2");
             return Ok(None);
         };
         let entry = self.cursor.next_dup_val()?;
