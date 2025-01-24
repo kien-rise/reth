99c879ddea992afd80351e4d159954bb01917cb0
Kien Nguyen <kien@riselabs.xyz>
2025-01-24 16:03:09 +0700

    DEBUG: skip empty tries

--- crates/trie/common/src/updates.rs
+++ crates/trie/common/src/updates.rs
@@ -48,6 +48,9 @@ impl TrieUpdates {
         self.account_nodes.extend(exclude_empty_from_pair(other.account_nodes));
         self.removed_nodes.extend(exclude_empty(other.removed_nodes));
         for (hashed_address, storage_trie) in other.storage_tries {
+            if storage_trie.is_empty() {
+                continue;
+            }
             self.storage_tries.entry(hashed_address).or_default().extend(storage_trie);
         }
     }
@@ -62,12 +65,19 @@ impl TrieUpdates {
         ));
         self.removed_nodes.extend(exclude_empty(other.removed_nodes.iter().cloned()));
         for (hashed_address, storage_trie) in &other.storage_tries {
+            if storage_trie.is_empty() {
+                continue;
+            }
             self.storage_tries.entry(*hashed_address).or_default().extend_ref(storage_trie);
         }
     }
 
     fn extend_common(&mut self, other: &Self) {
-        self.account_nodes.retain(|nibbles, _| !other.removed_nodes.contains(nibbles));
+        if !other.removed_nodes.is_empty() {
+            for k in other.removed_nodes.iter() {
+                self.account_nodes.remove(k);
+            }
+        }
     }
 
     /// Insert storage updates for a given hashed address.
@@ -199,7 +209,11 @@ impl StorageTrieUpdates {
             self.removed_nodes.clear();
         }
         self.is_deleted |= other.is_deleted;
-        self.storage_nodes.retain(|nibbles, _| !other.removed_nodes.contains(nibbles));
+        if !other.removed_nodes.is_empty() {
+            for k in other.removed_nodes.iter() {
+                self.storage_nodes.remove(k);
+            }
+        }
     }
 
     /// Finalize storage trie updates for by taking updates from walker and hash builder.
