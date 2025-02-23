583fd6feefe0faca628592e0236ac2a7da2b611a
Kien Nguyen <kien@riselabs.xyz>
2025-02-23 10:53:14 +0700

    DEBUG: add ForwardInMemoryCursor::removed_keys

--- crates/trie/trie/src/forward_cursor.rs
+++ crates/trie/trie/src/forward_cursor.rs
@@ -1,3 +1,7 @@
+use std::hash::Hash;
+
+use alloy_primitives::map::HashSet;
+
 /// The implementation of forward-only in memory cursor over the entries.
 /// The cursor operates under the assumption that the supplied collection is pre-sorted.
 #[derive(Debug)]
@@ -6,13 +10,15 @@ pub struct ForwardInMemoryCursor<'a, K, V> {
     entries: &'a Vec<(K, V)>,
     /// The index where cursor is currently positioned.
     index: usize,
+    /// List of removed keys
+    removed_keys: HashSet<K>,
 }
 
-impl<'a, K, V> ForwardInMemoryCursor<'a, K, V> {
+impl<'a, K: Eq + Hash, V> ForwardInMemoryCursor<'a, K, V> {
     /// Create new forward cursor positioned at the beginning of the collection.
     /// The cursor expects all of the entries have been sorted in advance.
-    pub const fn new(entries: &'a Vec<(K, V)>) -> Self {
-        Self { entries, index: 0 }
+    pub fn new(entries: &'a Vec<(K, V)>, removed_keys: impl IntoIterator<Item = K>) -> Self {
+        Self { entries, index: 0, removed_keys: HashSet::from_iter(removed_keys) }
     }
 
     /// Returns `true` if the cursor is empty, regardless of its position.
@@ -23,16 +29,11 @@ impl<'a, K, V> ForwardInMemoryCursor<'a, K, V> {
 
 impl<K, V> ForwardInMemoryCursor<'_, K, V>
 where
-    K: PartialOrd + Clone,
+    K: PartialOrd + Clone + Eq + Hash,
     V: Clone,
 {
     /// Advances the cursor until the predicate satisfies or EOF.
     fn find(&mut self, ok: impl Fn(&K) -> bool) {
-        let mut expected = self.index;
-        while self.entries.get(expected).is_some_and(|(k, _)| !ok(k)) {
-            expected += 1;
-        }
-
         let mut step = 1usize;
         let mut halving = false;
         while step > 0 {
@@ -44,8 +45,6 @@ where
                 step /= 2;
             }
         }
-
-        assert_eq!(expected, self.index);
     }
 
     /// Returns the first entry from the current cursor position that's greater or equal to the
@@ -60,5 +59,10 @@ where
     pub fn next(&mut self, key: &K) -> Option<(K, V)> {
         self.find(|k| k > key);
         self.entries.get(self.index).cloned()
+    }
+
+    /// Is key removed.
+    pub fn is_removed(&self, key: &K) -> bool {
+        self.removed_keys.contains(key)
     }
 }
