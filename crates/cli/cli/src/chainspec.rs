a1d97842b4383cd564da41f81c2caf7c86772d13
Kien Nguyen <kien@riselabs.xyz>
2025-07-14 23:55:53 +0700

    DEBUG: inspect parse_genesis

--- crates/cli/cli/src/chainspec.rs
+++ crates/cli/cli/src/chainspec.rs
@@ -68,7 +68,9 @@ pub trait ChainSpecParser: Clone + Send + Sync + 'static {
 /// A helper to parse a [`Genesis`](alloy_genesis::Genesis) as argument or from disk.
 pub fn parse_genesis(s: &str) -> eyre::Result<alloy_genesis::Genesis> {
     // try to read json from path first
-    let raw = match fs::read_to_string(PathBuf::from(shellexpand::full(s)?.into_owned())) {
+    let t0 = shellexpand::full(s)?.into_owned();
+    println!("parse_genesis | s={s:?} t0={t0:?}");
+    let raw = match fs::read_to_string(PathBuf::from(t0)) {
         Ok(raw) => raw,
         Err(io_err) => {
             // valid json may start with "\n", but must contain "{"
